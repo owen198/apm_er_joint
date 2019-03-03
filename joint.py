3023002590b70d36939464968d7a76a4170b5f12
Owen <owen@Owende-MacBook-Pro.local>
2019-03-03 14:09:18 +0800

    fix json string to dict

--- joint.py
+++ joint.py
@@ -6,20 +6,18 @@ import os
 WISE_PAAS_INSTANCE = 'iii-cflab.com'
 ENDPOINT_SSO = 'portal-sso-develop'
 ENDPOINT_AFS = 'api-afs-develop'
+ENDPOINT_APM = ''
 
-payload = {
-        'username': os.environ['USERNAME'],
-        'password': os.environ['PASSWORD']
-}
+payload = dict()
+payload['username'] = os.environ['USERNAME']
+payload['password'] = os.environ['PASSWORD']
 resp = requests.post('https://' + ENDPOINT_SSO + '.' + WISE_PAAS_INSTANCE + '/v2.0/auth/native', 
                      json=payload,
                      verify=False)
 
-
-header = {
-        "content-type": "application/json",
-        "Authorization": "Bearer " + resp.json()['accessToken']
-}
+header = dict()
+header['content-type'] = 'application/json'
+header['Authorization'] = 'Bearer ' + resp.json()['accessToken']
 resp = requests.get('https://' + ENDPOINT_AFS + '.' + WISE_PAAS_INSTANCE + '/v2/instances', 
                     headers=header,
                     verify=False)
