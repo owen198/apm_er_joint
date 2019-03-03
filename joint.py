bda7d962f4f2a722c922794b9f9ff435b6c3e01d
Owen HT Lu <cfleu198@gmail.com>
2019-03-03 14:01:23 +0800

    Update joint.py

--- joint.py
+++ joint.py
@@ -3,16 +3,24 @@
 import requests
 import os
 
+WISE_PAAS_INSTANCE = 'iii-cflab.com'
+ENDPOINT_SSO = 'portal-sso-develop'
+ENDPOINT_AFS = 'api-afs-develop'
+
 payload = {
         'username': os.environ['USERNAME'],
         'password': os.environ['PASSWORD']
 }
-resp = requests.post('https://portal-sso-develop.iii-cflab.com/v2.0/auth/native', json=payload)
+resp = requests.post('https://' + ENDPOINT_SSO + '.' + WISE_PAAS_INSTANCE + '/v2.0/auth/native', 
+                     json=payload,
+                     verify=False)
 
 
 header = {
         "content-type": "application/json",
         "Authorization": "Bearer " + resp.json()['accessToken']
 }
-resp = requests.get("https://api-afs-develop.iii-cflab.com/v2/instances", headers=header)
+resp = requests.get('https://' + ENDPOINT_AFS + '.' + WISE_PAAS_INSTANCE + '/v2/instances', 
+                    headers=header,
+                    verify=False)
 print(resp.json())
