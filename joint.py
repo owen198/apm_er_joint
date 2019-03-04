781552e01082751a056332653ce9c66e2717a5c9
Owen HT Lu <cfleu198@gmail.com>
2019-03-04 16:50:26 +0800

    Update joint.py

--- joint.py
+++ joint.py
@@ -23,14 +23,14 @@ resp = requests.get('https://' + ENDPOINT_AFS + '.' + WISE_PAAS_INSTANCE + '/v2/
                     verify=False)
 print(resp.json())
 
-
+# HIST_RAW_DATA
 ENDPOINT_APM = 'api-apm-acniotsense-apmdemo'
 APM_HIST = 'https://' + ENDPOINT_APM + '.' + WISE_PAAS_INSTANCE + '/hist/raw/data'
 
 payload = dict()
-payload['nodeId'] = '272'
+payload['nodeId'] = '2274'
 payload['sensorType'] = 'monitor'
-payload['sensorName'] = 'cnc_predict'
+payload['sensorName'] = 'Velocity RMS band 8'
 payload['startTs'] = '2018-12-29T00:36:18.000Z'
 payload['endTs'] = '2018-12-30T00:36:58.000Z'
 
@@ -43,3 +43,21 @@ r = requests.get(APM_HIST,
                  headers=header,
                  verify=False)
 print(r.url)
+
+# HIST_EVENT
+payload = dict()
+APM_EVENT = 'https://' + ENDPOINT_APM + '.' + WISE_PAAS_INSTANCE + '/hist/event'
+payload['nodeId'] = '2274'
+payload['count'] = '100'
+payload['startTs'] = '2018-12-29T00:36:18.000Z'
+payload['endTs'] = '2018-12-30T00:36:58.000Z'
+
+r = requests.get(APM_EVENT, 
+                 params=payload, 
+                 headers=header,
+                 verify=False)
+print(r.url)
+
+# TODO
+# 1. Leak how to get topo
+# 2. Leak how to get sensor list
